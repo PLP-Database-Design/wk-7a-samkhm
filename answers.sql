0bf4669341e411d4f37bce8b6cfd934649fa85c9
Samuel Kimanth <samuelkimanthi02@gmail.com>
2025-05-07 10:51:39 +0300

    submitted assignment

--- answers.sql
+++ answers.sql
@@ -1,36 +1,36 @@
--- 1.
+-- 1. Question 1NF
 
 -- Table for orders and customers
 CREATE TABLE customerDetails (
-    orderID INT PRIMARY KEY,
-    customerName VARCHAR(50) NOT NULL
+orderID INT PRIMARY KEY,
+customerName VARCHAR(50) NOT NULL
 );
 
 -- Table for individual products in each order
 CREATE TABLE productTable (
-    productID INT PRIMARY KEY,
-    orderID INT,
-    productName VARCHAR(50) NOT NULL,
-    FOREIGN KEY (orderID) REFERENCES customerDetails(orderID)
+productID INT PRIMARY KEY,
+orderID INT,
+productName VARCHAR(50) NOT NULL,
+FOREIGN KEY (orderID) REFERENCES customerDetails(orderID)
 );
 
--- 2.
+-- 2. Question 2NF
 CREATE TABLE CustomerDetails(
-    customerId INT PRIMARY KEY,
-    customerName VARCHAR(50) NOT NULL
+customerId INT PRIMARY KEY,
+customerName VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Product(
-    productId INT PRIMARY KEY,
-    productName VARCHAR(50) NOT NULL
+productId INT PRIMARY KEY,
+productName VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE orderDetails(
-    orderId INT PRIMARY KEY,
-    productId INT,
-    Quantity INT NOT NULL,
-    customerId INT,
-    FOREIGN KEY (customerId) REFERENCES CustomerDetails(customerId),
-    FOREIGN KEY (productId) REFERENCES Product(productId)
+orderId INT PRIMARY KEY,
+productId INT,
+Quantity INT NOT NULL,
+customerId INT,
+FOREIGN KEY (customerId) REFERENCES CustomerDetails(customerId),
+FOREIGN KEY (productId) REFERENCES Product(productId)
 );
